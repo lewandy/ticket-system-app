7c07198a65b8365bfee4cf9901388d1604c76203
Lewandy DIlone <lewandydilone1@live.com>
2019-12-04 17:58:13 -0400

    routes refactored

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
-import Vue from "vue"
-import Vuex from "vuex"
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 //Import modules
-import { auth } from "./modules/AuthModule"
-import { notifications } from "./modules/NotificationModule"
+import { auth } from "./modules/auth/AuthModule";
+import { notifications } from "./modules/auth/NotificationModule";
+
+import { employee } from "./modules/employee/employee";
 
 export default new Vuex.Store({
-    modules: {
-        auth,
-        notifications
-    }
-})
+  modules: {
+    auth,
+    notifications,
+    employee
+  }
+});
