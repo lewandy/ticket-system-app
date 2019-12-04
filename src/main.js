89dfb3a279bbc6da6aeb6e46ebeb24cb96417105
Lewandy DIlone <lewandydilone1@live.com>
2019-12-04 17:55:09 -0400

    add fonts to app

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import router from "@/routes/"
-import store from "@/store"
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import router from "@/routes/";
+import store from "@/store";
+import "./registerServiceWorker";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
