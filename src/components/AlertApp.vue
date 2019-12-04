c10965ea09a98e6b977c08673758974f40d21ca5
Lewandy DIlone <lewandydilone1@live.com>
2019-12-04 17:55:55 -0400

    notifiation component added

---
+++ src/components/AlertApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-alert dismissible v-model="alert.show" :type="alert.type || 'info'">{{ alert.message }}</v-alert>
+</template>
+
+<script>
+export default {
+  computed: {
+    alert() {
+      return this.$store.getters["notifications/notification"];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
