2bf75bac75715880f0126ffbf1b170a51b69010a
Lewandy DIlone <lewandydilone@gmail.com>
2019-12-04 09:55:27 -0400

    setup layout and remove example things

--- src/layouts/menu.js
+++ src/layouts/menu.js
@@ -1,32 +1,20 @@
 export default [
-    {
-        icon: "mdi-home",
-        text: "Inicio",
-        href: "/dashboard",
-        id: 1000
-    },
-    {
-        icon: "mdi-account-circle",
-        text: "Informacion personal",
-        href: "/personal-info",
-        id: 1001
-    },
-    {
-        icon: "mdi-calendar-clock",
-        text: "Preseleccion",
-        href: "/preselection",
-        id: 1003
-    },
-    {
-        icon: "mdi-account-badge-alert",
-        text: "Consulta de calificacion",
-        href: "/grades",
-        id: 1004
-    },
-    {
-        icon: "mdi-account-cash",
-        text: "Estado de cuenta",
-        href: "/account-status",
-        id: 1005
-    },
-]
+  {
+    icon: "mdi-home",
+    text: "Inicio",
+    href: "/tickets",
+    id: 1000
+  },
+  {
+    icon: "mdi-account-circle",
+    text: "Employee",
+    href: "/employees",
+    id: 1001
+  },
+  {
+    icon: "mdi-account-circle",
+    text: "Reports",
+    href: "/employees",
+    id: 1002
+  }
+];
